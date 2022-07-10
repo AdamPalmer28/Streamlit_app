8eb14803a9fd9078c3118f5e4e396c6461b9d2c6
AdamPalmer28 <93833080+AdamPalmer28@users.noreply.github.com>
2022-07-10 21:34:28 +0100

    Intial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+def main():
+    x=5
+    print(x**2)
+    print("Hello world")
+    pass
+
+if __name__ == '__main__':
+    main()
