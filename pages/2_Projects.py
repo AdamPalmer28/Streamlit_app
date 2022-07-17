b997252e1c5b03a2e2b749d0cee6c5a78b0a068a
AdamPalmer28 <93833080+AdamPalmer28@users.noreply.github.com>
2022-07-17 19:32:14 +0100

    page updates

--- pages/2_Projects.py
+++ pages/2_Projects.py
@@ -7,5 +7,10 @@ from page_class import *
 
 
 
-graphs_p = web_page("Welcome to my graphs page", "Graphs")
+graphs_p = web_page("My current project", "Projects")
 graphs_p.run()
+
+tab1, tab2, tab3, tab4 = st.tabs(["Algorithm visualiser","Chess engine", "3D game engine", "Connect 4 neural network"])
+
+with tab1:
+    st.header("Number sorting algorithm")
