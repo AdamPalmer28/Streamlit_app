3441956b46404aaa1ce9d1520d985b0f5774b035
AdamPalmer28 <93833080+AdamPalmer28@users.noreply.github.com>
2022-07-17 15:50:55 +0100

    Masters page

--- pages/3_Masters_Research.py
+++ pages/3_Masters_Research.py
@@ -11,12 +11,6 @@ mast_p = web_page("K-Hyperplane Clustering problem", "Masters")
 mast_p.run()
 
 
-#st.write("Demo text")
-
-#st.image("pages/resources/3_resources/Rot3.jpg")
-
-displayPDF("pages/HyperplaneClustering.pdf")
-
 
 if st.button("View PDF"):
     displayPDF("pages/resources/3_resources/HyperplaneClustering.pdf")
