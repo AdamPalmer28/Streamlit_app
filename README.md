eb18827a93557ef41350a4832831a1b34f15d727
AdamPalmer28 <93833080+AdamPalmer28@users.noreply.github.com>
2022-07-24 20:30:55 +0100

    README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Exploring Streamlit 
+
+This is a explorative project to show case the features of streamlit to help inspire future porjects.
+I will be using this streamlit app as a personal website about me, while also creating a portfolio of 
+streamlit tools/scripts. 
+
+The site is hosted 24/7 through streamlit sharing:
+https://adampalmer28-streamlit-app-home-lk5h00.streamlitapp.com/
+
--- pages/2_Projects.py
+++ pages/2_Projects.py
@@ -10,7 +10,33 @@ from page_class import *
 graphs_p = web_page("My current project", "Projects")
 graphs_p.run()
 
-tab1, tab2, tab3, tab4 = st.tabs(["Algorithm visualiser","Chess engine", "3D game engine", "Connect 4 neural network"])
+av_tab, chess_tab, rubix_tab, con4_tab = st.tabs(["Algorithm visualiser","Chess engine", "3D game engine", \
+                                    "Connect 4 neural network"])
 
-with tab1:
-    st.header("Number sorting algorithm")
+with av_tab:
+    st.subheader("Number sorting algorithm")
+
+
+with chess_tab:
+    st.subheader("Python - Chess engine")
+
+
+with rubix_tab:
+    st.subheader("My 3D engine")
+
+    st.write()
+
+
+with con4_tab:
+    st.subheader("Connect 4 AI")
+
+    st.write("")
+
+
+with st.expander("Potential future projects:"):
+
+    fp_text = """
+    * Webscraping 
+    * LBlockduko 
+    """
+    st.markdown(fp_text, unsafe_allow_html = True)
